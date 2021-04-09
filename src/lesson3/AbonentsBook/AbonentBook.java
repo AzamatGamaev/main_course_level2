e61601ce45106031c9bfb4b1de5cae48e6413363
GamaevAM <azamatgamaev@gmail.com>
2021-04-09 16:40:53 +0300

    Continue lesson 3.

--- src/lesson3/AbonentsBook/AbonentBook.java
+++ src/lesson3/AbonentsBook/AbonentBook.java
@@ -1,10 +1,28 @@
 package lesson3.AbonentsBook;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 public class AbonentBook {
     private Map<String, List<String>> abonentBook = new HashMap<>();
 
+    public void add(String name, String number) {
+        if (abonentBook.containsKey(name)) {
+            abonentBook.get(name).add(number);
+        } else {
+            List<String>list = new LinkedList<>();
+            list.add(number);
+            abonentBook.put(name, list);
+        }
+    }
+
+    public String get(String name){
+        if (abonentBook.containsKey(name)){
+            return abonentBook.get(name).toString();
+
+        }
+        return null;
+    }
 }
--- src/lesson3/AbonentsBook/Main.java
+++ src/lesson3/AbonentsBook/Main.java
@@ -1,5 +1,14 @@
 package lesson3.AbonentsBook;
 
 public class Main {
-
+    public static void main(String[] args) {
+        AbonentBook abonentBook = new AbonentBook();
+        abonentBook.add("Петя", "11");
+        abonentBook.add("Петя", "22");
+        abonentBook.add("Вася", "33");
+        abonentBook.add("Рома", "45");
+        System.out.println(abonentBook.get("Петя"));
+        System.out.println(abonentBook.get("Вася"));
+        System.out.println(abonentBook.get("Рома"));
+    }
 }
